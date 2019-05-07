b7160e279c0591da567627454d6459b72dd4e99b
Han Dole Kim <hdkimdev@gmail.com>
2019-05-07 13:09:24 -0700

    Initial Commit

---
+++ EasyToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyToDo
+//
+//  Created by Han Dole Kim on 5/7/19.
+//  Copyright © 2019 Han Dole Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
